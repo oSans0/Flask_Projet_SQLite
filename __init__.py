c1481d989e3a045137dd8b89c1eca984dd053006
oSans0 <liueric42@gmail.com>
2024-10-17 14:04:30 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,6 +49,16 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
+@app.route('/fiche_nom/<string:post_nom>')
+def Readnom(post_nom):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_nom,))
+    data = cursor.fetchall()
+    conn.close()
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
 @app.route('/consultation/')
 def ReadBDD():
     conn = sqlite3.connect('database.db')
